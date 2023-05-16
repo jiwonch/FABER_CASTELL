8fc7601fd2bf791270de5970f94c795719f9f8e6
jiwonch <rizjw26@gmail.com>
2023-05-16 17:37:39 +0900

    Fix : main js 연결되는 class 이름
    - 수정 #1 : main js에 사용되는 class 이름 네이밍 규칙에 맞게 변경

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // toggle click event 발생 시, main menu open
 $(document).ready(function(){
-    $('#tgl_mn').click(function(){
-        $('#full_nav_wrap').show();
+    $('.tglMn').click(function(){
+        $('.fullNav').show();
     });
 });
