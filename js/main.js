97cbf5d2f597a06543565b91e8aa37e1d004bfe4
권혁성 <kent.kwon@5gamstudio.com>
2023-05-16 21:49:57 +0900

    fix : 햄버거 버튼 및 메뉴 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 // toggle click event 발생 시, main menu open
 $(document).ready(function(){
     $('.tglMn').click(function(){
-        $('.fullNav').show();
+        $('.fullNav').toggle();
+        $('#tgl_mn').css('z-index', '9999');
+    });
+
+    $(document).click(function(event){
+        var target = $(event.target);
+        if (!target.closest('.tglMn').length) {
+            $('.fullNav').hide();
+            $('#tgl_mn').css('z-index', '1');
+        }
     });
 });
